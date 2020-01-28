24788cf133d71fa1c6c31d3a16361358f3aea1b7
Lúcio Leandro <lucio.leo85@gmail.com>
2020-01-28 17:28:18 -0300

    RestTemplate configuration

--- store/src/main/java/br/com/microservice/store/service/BuyService.java
+++ store/src/main/java/br/com/microservice/store/service/BuyService.java
@@ -1,5 +1,6 @@
 package br.com.microservice.store.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,11 @@ import br.com.microservice.store.domain.SupplierInfo;
 
 @Service
 public class BuyService {
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void saveBuy(Order order) {
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<SupplierInfo> exchange = client.exchange("http://supplier/supplierinfo/" + order.getAddress().getState(), 
 				HttpMethod.GET, null, SupplierInfo.class);
 		
